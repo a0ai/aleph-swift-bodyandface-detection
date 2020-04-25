2d34518d5fe9b38726a50b8b024ed09d8b14020e
cpp.dvl <julian@alephzero.ai>
2020-04-25 02:06:50 -0500

    Switching Camera from Back to Front

--- FaceLasers/FaceDetectionViewController.swift
+++ FaceLasers/FaceDetectionViewController.swift
@@ -35,34 +35,92 @@ class FaceDetectionViewController: UIViewController {
 
   @IBOutlet var faceView: FaceView!
   @IBOutlet var laserView: LaserView!
-  @IBOutlet var faceLaserLabel: UILabel!
+  @IBOutlet var faceBodyLabel: UILabel!
   
-  let session = AVCaptureSession()
+  var captureSession: AVCaptureSession?
   var previewLayer: AVCaptureVideoPreviewLayer!
-  
+  var currentCaptureDevice: AVCaptureDevice?
+  var videoOutput: AVCaptureVideoDataOutput!
   let dataOutputQueue = DispatchQueue(
     label: "video data queue",
     qos: .userInitiated,
     attributes: [],
     autoreleaseFrequency: .workItem)
 
-  var faceViewHidden = false
+  var faceDetection = true
   
   var maxX: CGFloat = 0.0
   var midY: CGFloat = 0.0
   var maxY: CGFloat = 0.0
-
+   
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    configureCaptureSession()
-    
+    loadCamera()
+    /*configureCaptureSession(campos: AVCaptureDevice.Position.front)
     laserView.isHidden = true
-    
     maxX = view.bounds.maxX
     midY = view.bounds.midY
     maxY = view.bounds.maxY
+    session.startRunning()*/
+  }
+  
+  
+}
+
+// MARK: - Session and Camera Switching
+extension FaceDetectionViewController{
+  func getFrontCamera() -> AVCaptureDevice?{
+    guard let frontDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
+      return nil
+    }
+    return frontDevice
+  }
+  func getBackCamera() -> AVCaptureDevice?{
+    guard let backDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+      return nil
+    }
+    return backDevice
+  }
+  
+  func purgeSessionInputs(){
+    if (captureSession == nil) { captureSession = AVCaptureSession() }
+    for i : AVCaptureDeviceInput in (self.captureSession?.inputs as! [AVCaptureDeviceInput]){
+      self.captureSession?.removeInput(i)
+    }
+  }
+  func connectVideoInputToSession(){
+    //(Re)connect Capture Device to the session.
+    currentCaptureDevice = faceDetection ? getFrontCamera() : getBackCamera()
+    do {
+      let videoInput = try AVCaptureDeviceInput(device: currentCaptureDevice!)
+      captureSession!.addInput(videoInput)
+    } catch{
+      fatalError(error.localizedDescription)
+    }
     
-    session.startRunning()
+  }
+  func connectSessionToVideoOutput(){
+    // Create the video data output where the video frames will be gotten from.
+      
+    let videoOutput = AVCaptureVideoDataOutput()
+    videoOutput.setSampleBufferDelegate(self, queue: dataOutputQueue)
+    videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+    videoOutput.connection(with: .video)?.videoOrientation = .portrait
+      
+    //Connect Session to videoutput
+    if captureSession!.canAddOutput(videoOutput){
+      captureSession!.addOutput(videoOutput)
+    }
+    
+  }
+  func configurePreviewLayer(){
+    if previewLayer == nil {
+      previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+      previewLayer.videoGravity = .resizeAspectFill
+      previewLayer.frame = view.bounds
+      view.layer.insertSublayer(previewLayer, at: 0)
+    }
   }
 }
 
@@ -70,53 +128,38 @@ class FaceDetectionViewController: UIViewController {
 
 extension FaceDetectionViewController {
   @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
-    faceView.isHidden.toggle()
-    laserView.isHidden.toggle()
-    faceViewHidden = faceView.isHidden
-    
-    if faceViewHidden {
-      faceLaserLabel.text = "Body"
+    faceDetection = !faceDetection
+    print("Face Detection: ",faceDetection)
+    if faceDetection {
+      faceBodyLabel.text = "Face"
     } else {
-      faceLaserLabel.text = "Face"
+      faceBodyLabel.text = "Body"
     }
+    loadCamera()
   }
 }
 
 // MARK: - Video Processing methods
 
 extension FaceDetectionViewController {
-  func configureCaptureSession() {
-    // Define the capture device we want to use
-    guard let camera = AVCaptureDevice.default(.builtInWideAngleCamera,
-                                               for: .video,
-                                               position: .back) else {
-      fatalError("No front video camera available")
+  func loadCamera(){
+    
+    //Purge Session
+    purgeSessionInputs()
+    
+    //(Re)Connect Capture Device Input to the Session
+    connectVideoInputToSession()
+    
+    //(Re)Connect Session to Data Ouput
+    connectSessionToVideoOutput()
+    
+    //Set the view
+    configurePreviewLayer()
+    
+    //Capture Session start
+    DispatchQueue.main.async {
+      self.captureSession!.startRunning()
     }
-    
-    // Connect the camera to the capture session input
-    do {
-      let cameraInput = try AVCaptureDeviceInput(device: camera)
-      session.addInput(cameraInput)
-    } catch {
-      fatalError(error.localizedDescription)
-    }
-    
-    // Create the video data output
-    let videoOutput = AVCaptureVideoDataOutput()
-    videoOutput.setSampleBufferDelegate(self, queue: dataOutputQueue)
-    videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
-    
-    // Add the video output to the capture session
-    session.addOutput(videoOutput)
-    
-    let videoConnection = videoOutput.connection(with: .video)
-    videoConnection?.videoOrientation = .portrait
-    
-    // Configure the preview layer
-    previewLayer = AVCaptureVideoPreviewLayer(session: session)
-    previewLayer.videoGravity = .resizeAspectFill
-    previewLayer.frame = view.bounds
-    view.layer.insertSublayer(previewLayer, at: 0)
   }
 }
 
@@ -302,7 +345,7 @@ extension FaceDetectionViewController {
         return
     }
 
-    if faceViewHidden {
+    if faceDetection {
       updateLaserView(for: result)
     } else {
       updateFaceView(for: result)
